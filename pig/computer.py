237d185e384c112f6d45ac41343ed19cca57bce3
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-22 18:43:21 +0100

    fixed some error with dice

--- pig/computer.py
+++ pig/computer.py
@@ -6,6 +6,7 @@ class Computer:
         self.score = 0
         self.current_round_score = 0
         self.difficulty = None
+        self.dice = dice.Dice()
     
     def set_computer_difficulty(self, difficulty):
         self.difficulty = difficulty
@@ -15,7 +16,7 @@ class Computer:
             easy_list_of_dice_values = [1,1,2,2,3,4,5,6]
             return random.choice(easy_list_of_dice_values)
         elif self.difficulty == "medium":
-            return dice.Dice.roll()
+            return self.dice.roll()
         else:
             hard_list_of_dice_values = [1,2,3,4,4,5,5,6,6]
             return random.choice(hard_list_of_dice_values)
