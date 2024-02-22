8b6964d7d065265defd193e2ff263a09f4b8b9b4
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-22 17:03:45 +0100

    set player name function added

--- pig/game.py
+++ pig/game.py
@@ -4,3 +4,7 @@ class Game:
     def __init__(self) -> None:
         self.player_1 = player.Player()
         self.player_2 = player.Player()
+
+    def set_player_names(self, player_name):
+        self.player_1.name = player_name
+        self.player_2.name = "computer"
