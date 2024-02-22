42cf5946fa5ac885348e6ffbc1605aee618238d4
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-22 17:47:58 +0100

    updated dice.py

--- pig/dice.py
+++ pig/dice.py
@@ -1,10 +1,7 @@
 import random
 
 class Dice:
-
-    def __init__(self):
-        pass
-
-    def roll():
+    
+    def roll(self):
         dice_value = random.randint(1,6)
         return dice_value
