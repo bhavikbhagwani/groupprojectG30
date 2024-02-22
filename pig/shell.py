fead1527a34cfa030d75b4d5da4b66d1bc273d04
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-22 17:04:02 +0100

    command for changing name of player added

--- pig/shell.py
+++ pig/shell.py
@@ -16,3 +16,16 @@ class Shell(cmd.Cmd):
         "start a brand new game"
         self.game = Game()
         print("New game started")
+
+    def do_name(self, arg):
+        "set a new name for the player"
+        if self.game is None:
+            print("Please start a new game first. You can do this by typing 'start'")
+            return
+        if not arg:
+            print("Name was not provided. Type 'name' and your name afterwards please (e.g. name Larsson)")
+            return
+        
+        name = arg.strip()
+        self.game.set_player_names(name)
+        print(f"Name changed to {name}")
