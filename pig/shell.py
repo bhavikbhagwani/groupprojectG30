6de4354f3a7b33dc5768ea63fa05672df6cd1c28
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-22 18:03:36 +0100

    added hold command for user

--- pig/shell.py
+++ pig/shell.py
@@ -66,3 +66,19 @@ class Shell(cmd.Cmd):
         
         
         self.game.player_rolls()
+
+    def do_hold(self, _):
+        "player holds"
+        if self.game is None:
+            print("Please start a new game first. You can do this by typing 'start'")
+            return
+        if self.game.player_1.name is None:
+            print(f"Please provide a name first. Type 'name' and your name afterwards please (e.g. name Patrick).")
+            return
+        if self.game.computer.difficulty is None:
+            print(f"Difficulty is not yet set. Type 'difficulty' and the difficulty afterwards please (easy,medium,hard).")
+            return
+        
+        
+        
+        self.game.player_holds()
