f7c2d1586af12b469331be3669711744a472df99
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-22 17:12:58 +0100

    added command to change difficulty of game

--- pig/shell.py
+++ pig/shell.py
@@ -29,3 +29,25 @@ class Shell(cmd.Cmd):
         name = arg.strip()
         self.game.set_player_names(name)
         print(f"Name changed to {name}")
+
+    def do_difficulty(self, arg):
+        "set a difficulty for the game"
+        if self.game is None:
+            print("Please start a new game first. You can do this by typing 'start'")
+            return
+        if self.game.player_1.name is None:
+            print(f"Please provide a name first. Type 'name' and your name afterwards please (e.g. name Patrick).")
+            return
+        if not arg:
+            print("Difficulty was not provided. Type 'difficulty' and the difficulty afterwards please (easy,medium,hard)")
+            return
+        
+        difficulty = arg.strip()
+        if difficulty != "easy" and difficulty!= "medium" and difficulty!= "hard":
+            print(f"{arg} is not a valid difficulty level. Provide either easy, medium or hard")
+            return
+        
+        self.game.set_game_difficulty(difficulty)
+        print(f"Difficulty changed to {difficulty}")
+        print(f"You can now start to roll or hold!")
+        
