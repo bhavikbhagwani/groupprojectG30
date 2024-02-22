6f4a7d16c550fdd89a2235573def7c65a329263d
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-22 17:12:44 +0100

    added method for changing difficulty

--- pig/game.py
+++ pig/game.py
@@ -4,7 +4,11 @@ class Game:
     def __init__(self) -> None:
         self.player_1 = player.Player()
         self.player_2 = player.Player()
+        self.difficulty = None
 
     def set_player_names(self, player_name):
         self.player_1.name = player_name
         self.player_2.name = "computer"
+
+    def set_game_difficulty(self, difficulty):
+        self.difficulty = difficulty
