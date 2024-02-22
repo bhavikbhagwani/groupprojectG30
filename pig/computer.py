6d04f6576973a40a968bd152de78555cda45b18e
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-22 18:14:40 +0100

    added method for computer dice-roll

--- pig/computer.py
+++ pig/computer.py
@@ -1,3 +1,4 @@
+import random, dice
 
 class Computer:
     def __init__(self):
@@ -9,3 +10,12 @@ class Computer:
     def set_computer_difficulty(self, difficulty):
         self.difficulty = difficulty
     
+    def return_computer_rolled_dice_value(self):
+        if self.difficulty == "easy":
+            easy_list_of_dice_values = [1,1,2,2,3,4,5,6]
+            return random.choice(easy_list_of_dice_values)
+        elif self.difficulty == "medium":
+            return dice.Dice.roll()
+        else:
+            hard_list_of_dice_values = [1,2,3,4,4,5,5,6,6]
+            return random.choice(hard_list_of_dice_values)
