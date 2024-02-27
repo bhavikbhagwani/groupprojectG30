c9058835baed008a4e709d00f8bda51157880e1c
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-02-27 14:56:19 +0100

    did some pylint modifications

--- pig/computer.py
+++ pig/computer.py
@@ -1,14 +1,18 @@
 """
-Computer class
-This class instantiates the Computer
+Computer class.
+
+This class instantiates the Computer.
 and contains some methods to set difficulty of the computer
 and return a dice value according to the computer's difficulty
 """
 
-import random, dice
+import random
+import dice
 
 class Computer:
+    """computer class"""
     def __init__(self):
+        """init method"""
         self.name = "computer"
         self.score = 0
         self.current_round_score = 0
@@ -17,14 +21,17 @@ class Computer:
         self.num_rounds = 0
     
     def set_computer_difficulty(self, difficulty):
+        """setter method to set difficulty"""
         self.difficulty = difficulty
     
     def return_computer_rolled_dice_value(self):
+        """method to return dice_value by difficulty"""
         if self.difficulty == "easy":
-            easy_list_of_dice_values = [1,1,2,2,3,4,5,6]
+            easy_list_of_dice_values = [1, 1, 2, 2, 3, 4, 5, 6]
             return random.choice(easy_list_of_dice_values)
         elif self.difficulty == "medium":
             return self.dice.roll()
         else:
-            hard_list_of_dice_values = [1,2,3,4,4,5,5,6,6]
+            hard_list_of_dice_values = [1, 2, 3, 4, 4, 5, 5, 6, 6]
             return random.choice(hard_list_of_dice_values)
+        
--- pig/dice.py
+++ pig/dice.py
@@ -1,12 +1,15 @@
 """
-Dice class
-This class has one job which is to roll a dice and return its value
+Dice class.
+
+This class has one job which is to roll a dice and return its value.
 """
 
 import random
 
 class Dice:
-    
+    """Example of Dice class"""
     def roll(self):
-        dice_value = random.randint(1,6)
+        """Roll a dice once and return the value."""
+        dice_value = random.randint(1, 6)
         return dice_value
+    
