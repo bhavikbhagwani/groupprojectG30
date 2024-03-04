c2c9360ef32c85f31755e1efca0e0e7c23233ae8
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-03-04 14:10:03 +0100

    added logic for updating frequency

--- pig/player.py
+++ pig/player.py
@@ -17,7 +17,22 @@ class Player:
         self.current_round_score = 0
         self.num_rounds = 0
         self.player_dice = dice.Dice()
+        self.frequency = [0, 0, 0, 0, 0, 0]
 
     def roll(self):
         """Return rolling value from player."""
-        return self.player_dice.roll()
+        dice_value = self.player_dice.roll()
+        if dice_value == 1:
+            self.frequency[0] += 1
+        elif dice_value == 2:
+            self.frequency[1] += 1
+        elif dice_value == 3:
+            self.frequency[2] += 1
+        elif dice_value == 4:
+            self.frequency[3] += 1
+        elif dice_value == 5:
+            self.frequency[4] += 1
+        else:
+            self.frequency[5] += 1
+        
+        return dice_value
